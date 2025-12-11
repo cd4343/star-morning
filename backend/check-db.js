88bbb02e6f1432747b6c30e626c98cb33fac6439
cd4343 <zhengruiye@gmail.com>
2025-12-11 14:24:37 +0800

    chore: 删除临时数据库修复脚本

--- backend/check-db.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('../stellar.db');
-db.get("SELECT sql FROM sqlite_master WHERE type='table' AND name='user_inventory'", (err, row) => {
-  console.log("Current table definition:");
-  console.log(row?.sql);
-  db.close();
-});
-
